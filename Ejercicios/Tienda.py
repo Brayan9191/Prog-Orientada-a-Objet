2c0baadc5cd181ea182119b6385ebf817f7bdf0f
Brayan Cespedes <brayanc082006@gmail.com>
2025-09-22 12:50:34 -0500

    Add files via upload

--- Ejercicios/Tienda.py
+++ Ejercicios/Tienda.py
@@ -2,22 +2,44 @@ class Tienda:
     def __init__(self, nom_Tienda, nom_Producto, Precio_Base, Descuento):
         self.Tienda = nom_Tienda
         self.Producto = nom_Producto
-        self.Precio = Precio_Base
+        self.Precio_Base = Precio_Base
         self.Descuento = Descuento
+        self.Precio_Final = 0
         
-        if(Precio_Base >= 200):
-            Descuento = ((Precio_Base / 100) * 15)
-            
-        if(Precio_Base < 200 & Precio_Base > 100):
-            Descuento = ((Precio_Base / 100) * 10)
-            
-        if(Precio_Base < 100):
-            Descuento = ((Precio_Base / 100) * 5)
+    #   Convertir porcentaje segun descuento
+        Descuento_Decimal = self.Descuento/100
         
-
-Tienda1 = Tienda("D1", "Queso", 1200)
-Tienda2 = Tienda("D1", "Queso", 85)
-Tienda3 = Tienda("D1", "Queso", 150)
-
-print(Tienda1.Tienda, Tienda1.Producto, Tienda1.Precio, Tienda1.Descuento)
+    #   Operaacion aritmetica de precio con el descuento
+        self.Precio_Final = self.Precio_Base * (1- Descuento_Decimal)
+    
+    #   Condicionales    
+        if Precio_Base >= 200:
+            print("Producto de alto valor, se aplica un descuento adicional del 15% al precio final")
+            self.Precio_Final = self.Precio_Final * 0.85
             
+        elif(Precio_Base < 200 & Precio_Base > 100):
+            print("Producto de mediano valor, se aplica un descuento adicional del 10% al precio final")
+            self.Precio_Final = self.Precio_Final * 0.90
+            
+        elif(Precio_Base < 100):
+            print("Producto de bajo valor, se aplica un descuento adicional del 5% al precio final")
+            self.Precio_Final = self.Precio_Final * 0.95
+            
+    def mostrar_info(self):
+        print ("\n---- DETALLES DEL PRODUCTO ----")
+        print (f" Tienda: {self.Tienda}")                #   Siempre se inicia los corchetes del print con "(f"")" para poder ingresar variables
+        print (f" Producto: {self.Producto}")
+        print (f" Precio base: {self.Precio_Base}")
+        print (f" Descuento aplicado al producto: {self.Descuento}")
+        print (f" Total a pagar ${self.Precio_Final}")
+        print ("----------\n")
+
+Producto1 = Tienda("D1", "Queso", 300, 0)
+Producto2 = Tienda("Tecno_UNIMINUTO", "Portatil", 500, 10)
+Producto3 = Tienda("D1", "Mouse", 50, 5)
+Producto4 = Tienda("Tecno_UNIMINUTO", "Teclado", 99, 6)
+
+Producto1.mostrar_info()
+Producto2.mostrar_info()
+Producto3.mostrar_info()
+Producto4.mostrar_info()
